c68b402a22236ff3c857230883c76a5ce9f85cdf
vikitaotiz <victorotieno598@gmail.com>
2024-05-01 15:52:40 +0300

    Second commit

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const io = new Server(server,{ cors: {
 }});
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('<h1>Realtime Relay Engine</h1>');
 });
 
 app.get('/notify', (req, res) => {
 
-  io.emit("test_ch", "Payment Notification Sent" )
+  io.emit("test_ch", "Payment Notification Sent")
 
   res.json({ message: "Payment Notification Sent"});
 });
@@ -24,14 +24,6 @@ io.on('connection', (socket) => {
   console.log("front connected");
   
 });
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-
-//     socket.on("test_ch", () => {
-//         console.log('Kuom');
-//     })
-// });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
